2af9cd080baa905808f6c7d28bff8ee00db3c157
stevenaw <sweerdenburg@gmail.com>
2021-07-30 06:26:48 -0400

    Update xlsx to 0.17.0

--- src/loaders/xlsx.js
+++ src/loaders/xlsx.js
@@ -5,13 +5,21 @@ class XlsxLoader {
     const workbook = XLSX.readFile(filePath);
     const firstSheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[firstSheetName];
-
+	
     const sheetContent = XLSX.utils.sheet_to_json(worksheet, { header: 'A' });
-    return sheetContent.map(row => ({
-      url: row.A.trim(),
-      expectedRedirect: row.B.trim(),
-      maxHops: row.C ? (parseInt(row.C.trim(), 10) || undefined) : undefined
-    }));
+    
+	console.log(sheetContent);
+	return sheetContent.map(row => {
+		let maxHops = Number.isInteger(row.C) ?
+						row.C :
+						(parseInt(row.C.toString().trim(), 10) || undefined);
+						
+		return {
+		  url: row.A.trim(),
+		  expectedRedirect: row.B.trim(),
+		  maxHops: maxHops
+		}
+	});
   }
 }
 
